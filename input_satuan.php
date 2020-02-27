04a070c373b705b76c9f77b9af7b6bd03f0d27d3
novi <56795666+novinoppi@users.noreply.github.com>
2020-02-27 13:31:38 +0700

    Add files via upload

---
+++ input_satuan.php
@@ -0,0 +1,35 @@
+<?php
+include('koneksi.php');
+if(isset($_POST['save'])){
+	$query_barang="imsert into barang(id_barang, nama_barang, kategori_id, saturan_id)
+	values('".$_POST['id_barang']."',
+	'".$_POST['nama_barang']."',
+	'".$_POST['kategori_id']."',
+	'".$_POST['satuan_id']."')";
+	$proses=mysql_query($save);
+	if($proses){
+		header("location:tampil_barang.php");
+	}else{
+		echo mysql_errno();
+	}
+}
+?><form method="post">
+<table border="1">
+<tr>
+<td> id barang</td>
+<td><input type="text"
+name="id_barang"></td>
+</tr>
+<tr>
+	<td>nama</td>
+	<td><input type="text"
+name="nama_barang"></td>
+	</tr>
+	<tr>
+	<td></td>
+	<td><input type="submit"
+name="save"></td>
+</tr>
+</table>
+</form?
+<a href="menu.php">menu utama</a>
