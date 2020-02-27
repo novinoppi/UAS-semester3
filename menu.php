f9ba84f82bd50774f870fe801460439bd8448e3c
novi <56795666+novinoppi@users.noreply.github.com>
2020-02-27 13:34:25 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,15 @@
+<?php
+include('koneksi.php');
+?>
+<div class="container">
+	<h1>menu utama halaman uas</h1>
+	<ul>
+	<li><a href="input_brg.php">master barang</a></li>
+	<li><a href="input_satuan.php">master satuan</a></li>
+	<li><a href="input_kategori.php">master kategori</a></li>
+	<li><a href="tampil_barang.php">tampil barang</a></li>
+	<li><a href="tampil_kategori.php">tampil kategori</a></li>
+	<li><a href="tampil_transaksi.php">tampil transaksi</a></li>
+	<li><a href="laporan_transaksi_barang_perkategori.php">laporan transaksi</a></li>
+	</ul>
+</div>
